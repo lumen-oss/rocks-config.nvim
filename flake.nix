68e0952d5ea7666ddd7743cebee9cd1780d61ed0
Marc Jakobi <marc@jakobi.dev>
2026-09-05 18:16:16 +0200

    docs: rewrite for lux-config.nvim, nix git-hook for docgen
    Signed-off-by: Marc Jakobi <marc@jakobi.dev>

--- flake.nix
+++ flake.nix
@@ -9,53 +9,63 @@
   inputs = {
     nixpkgs.url = "github:NixOS/nixpkgs/nixos-unstable";
     lux.url = "github:lumen-oss/lux";
+    git-hooks = {
+      url = "github:cachix/git-hooks.nix";
+      inputs.nixpkgs.follows = "nixpkgs";
+    };
   };
 
-  outputs =
-    inputs@{
-      self,
-      ...
-    }:
-    let
-      foreach =
-        xs: f:
-        with inputs.nixpkgs.lib;
-        foldr recursiveUpdate { } (
-          if isList xs then
-            map f xs
-          else if isAttrs xs then
-            mapAttrsToList f xs
-          else
-            throw "foreach: expected list or attrset but got ${typeOf xs}"
+  outputs = inputs @ {self, ...}: let
+    foreach = xs: f:
+      with inputs.nixpkgs.lib;
+        foldr recursiveUpdate {} (
+          if isList xs
+          then map f xs
+          else if isAttrs xs
+          then mapAttrsToList f xs
+          else throw "foreach: expected list or attrset but got ${typeOf xs}"
         );
-    in
-      foreach inputs.nixpkgs.legacyPackages (
-        system: pkgs:
-        let
-          pkgs = inputs.nixpkgs.legacyPackages.${system}.extend inputs.lux.overlays.default;
-        in
-        {
-          devShells.${system}.default = pkgs.mkShell {
-            name = "lux-config.nvim-devShell";
-            buildInputs =
-              with pkgs;
-              [
-                lux-cli
-                luajit
-                rustc
-                cargo
-                stylua
-                lua52Packages.luacheck
-                emmylua-ls
-              ];
-            shellHook = ''
-              if command -v nvim >/dev/null 2>&1; then
-                export VIMRUNTIME="$(nvim --clean --headless -c 'lua io.write(vim.env.VIMRUNTIME)' +q)";
-              else
-                export VIMRUNTIME="${pkgs.neovim-unwrapped}/share/nvim/runtime";
-              fi
-            '';
+  in
+    foreach inputs.nixpkgs.legacyPackages (
+      system: pkgs: let
+        pkgs = inputs.nixpkgs.legacyPackages.${system}.extend inputs.lux.overlays.default;
+
+        pre-commit-check = inputs.git-hooks.lib.${system}.run {
+          src = self;
+          hooks = {
+            alejandra.enable = true;
+            stylua.enable = true;
+            luacheck.enable = true;
+            editorconfig-checker.enable = true;
+            panvimdoc = {
+              enable = true;
+              name = "panvimdoc";
+              entry = "${pkgs.panvimdoc}/bin/panvimdoc --project-name lux-config --toc true --treesitter true --demojify true --description ' Configure Neovim plugins from lux.toml ' --input-file";
+              files = "docs/.*\\.md$";
+            };
           };
-        }
-      );
+        };
+      in {
+        devShells.${system}.default = pkgs.mkShell {
+          name = "lux-config.nvim-devShell";
+          buildInputs = with pkgs; [
+            lux-cli
+            luajit
+            rustc
+            cargo
+            stylua
+            lua52Packages.luacheck
+            emmylua-ls
+          ];
+          shellHook = ''
+            ${pre-commit-check.shellHook}
+            if command -v nvim >/dev/null 2>&1; then
+              export VIMRUNTIME="$(nvim --clean --headless -c 'lua io.write(vim.env.VIMRUNTIME)' +q)";
+            else
+              export VIMRUNTIME="${pkgs.neovim-unwrapped}/share/nvim/runtime";
+            fi
+          '';
+        };
+      }
+    );
 }
